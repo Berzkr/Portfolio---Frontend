fa1b1cb092a65de7393d48cd0c2c903348926219
Berzkr <99560925+Berzkr@users.noreply.github.com>
2022-08-04 18:19:03 -0300

    creación del routing
    se crea el componente home y el componente login y mediante el routing se enruta el botón login con el componente login

--- PortfolioWeb_MarianoSuppo/Frontend/src/app/app.module.ts
+++ PortfolioWeb_MarianoSuppo/Frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HysSkillsComponent } from './components/hys-skills/hys-skills.component
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -27,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysSkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
